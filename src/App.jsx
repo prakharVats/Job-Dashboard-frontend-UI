5ce3d8be0c56a5661d628e30778404ad72765fa4
prakharVats <vatsprakhar91@gmail.com>
2024-09-24 12:00:39 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import LoginPage from "./Components/LoginPage"
+
+
+function App() {
+  return (
+    <>
+      <LoginPage/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/LoginPage.jsx
@@ -0,0 +1,10 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <Box>This is the login page</Box>
+  )
+}
+
+export default LoginPage
