d39dfd1023ca5467313c2af7af22003fe6d4bb1c
prakharVats <vatsprakhar91@gmail.com>
2024-09-26 11:09:34 +0530

    Table introduced

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,7 @@
 import { Box, Stack, Typography } from '@mui/material'
 import Pie from './Graphs/Pie.jsx'
 import Line from './Graphs/LineGraph.jsx'
-
+import Table from "./Minor/Table.jsx"
 const border= {
   border:"1px solid black"
 }
@@ -10,12 +10,13 @@ const border= {
 //   fontWeight : "bold"
 // }
 const Home = () => {
-  const RowD = ["S/n" , "Company" , "Salary" , "City" , "Type"];
+  // const RowD = ["S/n" , "Company" , "Salary" , "City" , "Type"];
   const DummyData = [
-    {no : "1" , company : "Microsoft" , Salary : "42Lpa" , City:"Bengalore" , Type:"On Field"},
-    {no : "2" , company : "Amazon" , Salary : "72Lpa" , City:"NaN" , Type:"Remote"},
-    {no : "3" , company : "Google" , Salary : "1.2Cr" , City:"Pune" , Type:"On Field"},
+    {No : "1" , Company : "Microsoft" , Salary : "42Lpa" , City:"Bengalore" , Type:"On Field"},
+    {No : "2" , Company : "Amazon" , Salary : "72Lpa" , City:"NaN" , Type:"Remote"},
+    {No : "3" , Company : "Google" , Salary : "1.2Cr" , City:"Pune" , Type:"On Field"},
   ]
+  const TitleData = [{No : "S/n" , Company : "Company" , Salary : "Salary" , City:"City" , Type:"Type"},]
   return (
     <Box sx={{height:"200vh"}}>
         <Stack direction="row">
@@ -27,33 +28,42 @@ const Home = () => {
           {/* Uploads */}
           <Box sx={{height:"300px" , width:"600px" , p:"20px"  , ...border}}>
             {/* first row */}
-              <Box bgcolor="#d9d9d9">
-              < Drafts head = {1} RowD={RowD}/>
-              </Box>
-
-            {/* following rows */}
-              {/* Dummy data */}
-              {/* <Drafts head = {0} RowD={DummyData}/> */}
+            
+           
           </Box>  
+        </Stack>
+        <Stack direction="row">
+        <Table/>
+        <Table/>
         </Stack>
     </Box>
   )
 }
      
 
-const Drafts = ({RowD , head}) =>{
+const Rows = ({e}) =>{
+  console.log(e.No);
   return <>
   <Stack direction="row" spacing={10} padding={2}>
-  {RowD.map(e => <OneRow key={e} text = {e} head = {head}/>)}
-  </Stack>
+    <Typography sx={{fontWeight : `${e.No === "S/n" ? "bold" : ""}`}}>{e.No}</Typography>
+    <Typography sx={{fontWeight : `${e.No === "S/n" ? "bold" : ""}`}}>{e.Company}</Typography>
+    <Typography sx={{fontWeight : `${e.No === "S/n" ? "bold" : ""}`}}>{e.Salary}</Typography>
+    <Typography sx={{fontWeight : `${e.No === "S/n" ? "bold" : ""}`}}>{e.City}</Typography>
+    <Typography sx={{fontWeight : `${e.No === "S/n" ? "bold" : ""}`}}>{e.Type}</Typography>
+ </Stack>
+  </>
+}
+const Rows2 = ({e}) =>{
+  
+  <>
+  <Stack direction="row" spacing={10} padding={2}>
+  <Typography color='black'>hello</Typography>
+    <Typography>hello</Typography>
+    <Typography>hello</Typography>
+    <Typography>hello</Typography>
+    <Typography>hello</Typography>
+ </Stack>
   </>
 }
 
-const OneRow = ({text , head}) =>{
-  return (
-  <Typography sx={{fontWeight : `${head ? "bold" : ""}`}}>
-    {text}
-  </Typography>
-)
-}
 export default Home
